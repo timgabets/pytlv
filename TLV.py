4493e96f7729bd72c1721df5801edd965f53ce67
Tim Gabets <tim@gabets.ru>
2017-04-21 17:34:47 +0300

    test_tlv_parse_84_A5()

--- TLV.py
+++ TLV.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+import binascii
+from collections import OrderedDict
+
 class TLV:
-	def parse(self, data):
-		return None
+	def __init__(self, tags):
+		self.tags = tags
+		
+		self.tag_lengths = set()
+		for tag in self.tags:
+			self.tag_lengths.add(len(tag))
+
+	def parse(self, tlv_string):
+		parsed_data = OrderedDict()
+
+		for i in range(len(tlv_string)):
+			for tag_length in self.tag_lengths:
+				for tag in self.tags:
+					if tlv_string[i:i+tag_length] == tag:
+						value_length = int(tlv_string[i+tag_length:i+tag_length+2], 16)
+						value = tlv_string[i+tag_length+2 : i+tag_length+2+value_length*2]
+						parsed_data[tag] = value
+
+		return parsed_data
--- tests.py
+++ tests.py
@@ -5,10 +5,13 @@ from TLV import *
 
 class TestTLV(unittest.TestCase):
     def setUp(self):
-        self.tlv = TLV()
+        self.tlv = TLV(['84', 'A5'])
 
     def test_tlv_parse_empty_string(self):
-        self.assertEqual(self.tlv.parse(''), None)
+        self.assertEqual(self.tlv.parse(''), {})
+
+    def test_tlv_parse_84_A5(self):
+        self.assertEqual(self.tlv.parse('840E315041592E5359532E4444463031A5088801025F2D02656E'), {'84': '315041592E5359532E4444463031', 'A5': '8801025F2D02656E'})        
 
 if __name__ == '__main__':
     unittest.main()
