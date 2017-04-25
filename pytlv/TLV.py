449b0065601090d2eae07e7a63460e2bcda6dc10
Tim Gabets <tim@gabets.ru>
2017-04-25 12:43:46 +0300

    Added descriptions for bytes 2-5

--- pytlv/TLV.py
+++ pytlv/TLV.py
@@ -253,6 +253,10 @@ class TLV:
 
 		tvr_bit_names = {
 			1: ['RFU', 'SDA was selected', 'CDA failed', 'DDA failed', 'Card number appears on hotlist', 'ICC data missing', 'SDA failed', 'Offline data processing was not performed'],
+			2: ['RFU', 'RFU', 'RFU', 'New card', 'Requested service not allowed for card product', 'Application not yet effective', 'Expired application', 'Card and terminal have different application versions'],
+			3: ['RFU', 'RFU', 'On-line PIN entered', 'PIN entry required, PIN pad present, but PIN was not entered', 'PIN entry required, but no PIN pad present or not working', 'PIN try limit exceeded', '	Unrecognised CVM', 'Cardholder verification was not successful'],
+			4: ['RFU', 'RFU', 'RFU', 'Merchant forced transaction on-line', 'Transaction selected randomly of on-line processing', 'Upper consecutive offline limit exceeded', 'Lower consecutive offline limit exceeded', 'Transaction exceeds floor limit'],
+			5: ['RFU', 'RFU', 'RFU', 'RFU', 'Script processing failed after final Generate AC', 'Script processing failed before final Generate AC', 'Issuer authentication failed', 'Default TDOL Used']
 		}
 
 		for byte in range(1, 6):
